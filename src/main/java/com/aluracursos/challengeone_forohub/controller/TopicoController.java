9dbe41196b4ebfba937e32078f2cacfe4cda7efe
Sofia <sofialqz@gmail.com>
2024-07-24 17:52:23 -0300

    Actualiza return de los endpoints haciendo uso de la clase ResponseEntity para personalizar la respuesta HTTP de los métodos

--- src/main/java/com/aluracursos/challengeone_forohub/controller/TopicoController.java
+++ src/main/java/com/aluracursos/challengeone_forohub/controller/TopicoController.java
@@ -10,34 +10,41 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 
 @RestController
 @RequestMapping("/topicos")
 public class TopicoController {
     @Autowired
     private TopicoService topicoservicio;
-
+    // Registrar un tópico
     @PostMapping
-    public void registrarTopico(@RequestBody @Valid DatosTopico datos){
-        topicoservicio.registrarTopico(datos);
+    public ResponseEntity registrarTopico(@RequestBody @Valid DatosTopico datos,
+                                          UriComponentsBuilder uriComponentsBuilder){
+        return topicoservicio.registrarTopico(datos, uriComponentsBuilder);
     }
+    // Mostrar todos los tópicos
     @GetMapping
-    public Page<DatosMuestraTopicos> mostrarTopicos(@PageableDefault(size=2) Pageable paginacion){
+    public ResponseEntity<Page<DatosMuestraTopicos>> mostrarTopicos(@PageableDefault(size=2) Pageable paginacion){
         return topicoservicio.mostrarTopicos(paginacion);
     }
+    // Mostrar un tópico
     @GetMapping("/{id}")
-    public DatosMuestraTopicos obtenerTopico(@PathVariable Long id){
+    public ResponseEntity<DatosMuestraTopicos> obtenerTopico(@PathVariable Long id){
         return topicoservicio.obtenerTopico(id);
     }
+    // Actualizar un tópico
     @PutMapping("/{id}")
     @Transactional
-    public void actualizarTopico(@PathVariable Long id, @RequestBody @Valid DatosActualizaTopico datos){
-        topicoservicio.actualizarTopico(id, datos);
+    public ResponseEntity actualizarTopico(@PathVariable Long id, @RequestBody @Valid DatosActualizaTopico datos){
+        return topicoservicio.actualizarTopico(id, datos);
     }
+    // Eliminar un tópico
     @DeleteMapping("/{id}")
     @Transactional
-    public void eliminarTopico(@PathVariable Long id){
-        topicoservicio.eliminarTopico(id);
+    public ResponseEntity eliminarTopico(@PathVariable Long id){
+        return topicoservicio.eliminarTopico(id);
     }
 }
--- src/main/java/com/aluracursos/challengeone_forohub/service/TopicoService.java
+++ src/main/java/com/aluracursos/challengeone_forohub/service/TopicoService.java
@@ -8,47 +8,57 @@ import com.aluracursos.challengeone_forohub.repository.ITopicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.util.UriComponentsBuilder;
 
+import java.net.URI;
 import java.util.Optional;
 
 @Service
 public class TopicoService {
     @Autowired
     private ITopicoRepository topicoRepository;
-
     @PostMapping
-    public void registrarTopico(DatosTopico datos){
-        topicoRepository.save(new Topico(datos));
-    }
-    @GetMapping
-    public Page<DatosMuestraTopicos> mostrarTopicos(Pageable paginacion){
-        return topicoRepository.findAll(paginacion).map(DatosMuestraTopicos::new);
+    public ResponseEntity<DatosMuestraTopicos> registrarTopico(DatosTopico datos, UriComponentsBuilder uriComponentsBuilder){
+        Topico topico = topicoRepository.save(new Topico(datos));
+        DatosMuestraTopicos topicoRespuesta = new DatosMuestraTopicos(topico);
+        URI url = uriComponentsBuilder.path("/medicos/{id}").buildAndExpand(topico.getId()).toUri();
+        return ResponseEntity.created(url).body(topicoRespuesta);
     }
 
-    public DatosMuestraTopicos obtenerTopico(Long id){
+    @GetMapping
+    public ResponseEntity<Page<DatosMuestraTopicos>> mostrarTopicos(Pageable paginacion){
+        return ResponseEntity.ok(topicoRepository.findAll(paginacion).map(DatosMuestraTopicos::new));
+    }
+
+    public ResponseEntity<DatosMuestraTopicos> obtenerTopico(Long id){
         Optional<Topico> topico = topicoRepository.findById(id);
         if (topico.isPresent()){
             Topico topicoObtenido = topico.get();
-            return new DatosMuestraTopicos(topicoObtenido);
+            return ResponseEntity.ok(new DatosMuestraTopicos(topicoObtenido));
         }
         return null;
     }
 
-    public void actualizarTopico(Long id, DatosActualizaTopico datos){
+    public ResponseEntity<DatosMuestraTopicos> actualizarTopico(Long id, DatosActualizaTopico datos){
         Optional<Topico> topico = topicoRepository.findById(id);
         if (topico.isPresent()) {
             Topico topicoObtenido = topico.get();
             topicoObtenido.actualizarDatos(datos);
+            return ResponseEntity.ok(new DatosMuestraTopicos(topicoObtenido));
         }
+        return null;
     }
 
-    public void eliminarTopico(Long id) {
+    public ResponseEntity eliminarTopico(Long id){
         Optional<Topico> topico = topicoRepository.findById(id);
         if (topico.isPresent()){
             topicoRepository.deleteById(id);
+            return ResponseEntity.noContent().build();
         }
+        return null;
     }
 }
